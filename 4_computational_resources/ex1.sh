d80086c953e76d50f4af256fbe2b731ae213fe44
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-05-11 00:14:39 +0200

    upd

--- 4_computational_resources/ex1.sh
+++ 4_computational_resources/ex1.sh
@@ -5,6 +5,8 @@ set -x
 
 NS="compute"
 
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
 # Run on 3_kubeadm to get LimitRange
 # see "kubernetes in action" p405
 kubectl delete ns -l "compute=true"
@@ -14,7 +16,7 @@ kubectl label ns "$NS" "compute=true"
 kubectl config set-context $(kubectl config current-context) --namespace=$NS
 
 # Test network policies below
-KUBIA_DIR="/tmp/kubernetes-in-action"
+    KUBIA_DIR="/tmp/kubernetes-in-action"
 if [ ! -d "$KUBIA_DIR" ]; then
     git clone https://github.com/luksa/kubernetes-in-action.git /tmp/kubernetes-in-action
 
@@ -22,7 +24,7 @@ fi
 
 cd "$KUBIA_DIR/Chapter14"
 
-kubectl apply -f requests-pod.yaml
+kubectl apply -f "$KUBIA_DIR"/Chapter14/requests-pod.yaml
 sleep 5
 kubectl exec -it requests-pod top
 
@@ -39,14 +41,26 @@ kubectl delete po requests-pod-3
 kubectl get po
 kubectl delete pods requests-pod-4
 
-kubectl apply -f limited-pod.yaml
+kubectl apply -f "$KUBIA_DIR"/Chapter14/limited-pod.yaml
 sleep 5
 kubectl describe pod limited-pod
 kubectl exec -it limited-pod top
 
+# LimitRange
 kubectl apply -f $DIR/../3_authorization/manifest/local-storage.yaml
-kubectl apply -f limits.yaml
-kubectl apply -f limits-pod-too-big.yaml
-kubectl apply -f ../Chapter03/kubia-manual.yaml
+kubectl apply -f "$KUBIA_DIR"/Chapter14/limits.yaml
+kubectl apply -f "$KUBIA_DIR"/Chapter14/limits-pod-too-big.yaml && \
+    >&2 echo "ERROR this command should have failed"
+kubectl apply -f "$KUBIA_DIR"/Chapter03/kubia-manual.yaml
+
+# ResourceQuota
+kubectl apply -f "$KUBIA_DIR"/Chapter14/quota-cpu-memory.yaml
+kubectl describe quota
+# requests.storage is the overall max limit
+# https://kubernetes.io/docs/concepts/policy/resource-quotas/#storage-resource-quota
+# so there is an inconsistency in example
+kubectl apply -f "$KUBIA_DIR"/Chapter14/quota-storage.yaml
+kubectl apply -f "$KUBIA_DIR"/Chapter14/quota-object-count.yaml
+
 
 kubectl config set-context $(kubectl config current-context) --namespace=default
