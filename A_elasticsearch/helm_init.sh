00ed5d6e3b42acfe7e49171bfe0e0cac3888eb8f
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-05-10 17:05:44 +0200

    upd

--- A_elasticsearch/helm_init.sh
+++ A_elasticsearch/helm_init.sh
@@ -5,5 +5,6 @@ helm init
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'   
+sleep 5
 helm init --service-account tiller --upgrade
 
--- A_elasticsearch/install.sh
+++ A_elasticsearch/install.sh
@@ -4,9 +4,13 @@ set -x
 set -e
 
 # WARN does not work in dind
-# see https://platform9.com/blog/kubernetes-"$NS"-and-monitoring-the-elasticsearch-fluentd-and-kibana-efk-stack-part-2-elasticsearch-configuration/
+# see https://platform9.com/blog/kubernetes-logging-and-monitoring-the-elasticsearch-fluentd-and-kibana-efk-stack-part-2-elasticsearch-configuration/
 
 NS="logging"
+
+helm delete --purge elasticsearch || echo "elasticsearch not found"
+helm delete --purge fluentd || echo "fluentd not found"
+helm delete --purge kibana || echo "kibana not found"
 
 kubectl delete ns -l name="logging"
 kubectl create namespace "$NS"
@@ -22,11 +26,21 @@ helm repo add kiwigrid https://kiwigrid.github.io
 helm install --name fluentd --namespace "$NS" kiwigrid/fluentd-elasticsearch --set elasticsearch.host=elasticsearch-client."$NS".svc.cluster.local,elasticsearch.port=9200
 
 # Install Kibana
-helm install --name kibana --namespace "$NS" stable/kibana --set env.ELASTICSEARCH_URL=http://elasticsearch-client."$NS".svc.cluster.local:9200
+helm install --name kibana --namespace "$NS" stable/kibana --set env.ELASTICSEARCH_HOSTS=http://elasticsearch-client."$NS".svc.cluster.local:9200
 
 # Generate logs
 ./generate-log.sh > /dev/null &
 
 POD_NAME=$(kubectl get pods --namespace "$NS" -l "app=kibana,release=kibana" -o jsonpath="{.items[0].metadata.name}")
+
+# Wait for office:task-pv-pod to be in running state
+while true
+do
+    sleep 2
+    STATUS=$(kubectl get pods -n "$NS" "$POD_NAME" -o jsonpath="{.status.phase}")
+    if [ "$STATUS" = "Running" ]; then
+        break
+    fi
+done
 kubectl port-forward -n "$NS" "$POD_NAME" 5601&
 # In Kibana, go to "Discover", add "logstash*" index and "@timestamp" filter, then go to "Discover"
