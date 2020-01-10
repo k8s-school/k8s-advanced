f7a7fe0b305548ebdc5b7ded6994b34e5b3f3469
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-01-10 11:30:51 +0100

    upd

--- 3_policies/ex4-network.sh
+++ 3_policies/ex4-network.sh
@@ -61,7 +61,7 @@ PGSQL_IP=$(kubectl get pods -n network pgsql-postgresql-0 -o jsonpath='{.status.
 kubectl exec -n "$NS" -it nginx -- netcat -q 2 -nzv ${PGSQL_IP} 5432
 kubectl exec -n "$NS" -it nginx -- netcat -q 2 -zv pgsql-postgresql 5432
 kubectl exec -n "$NS" -it nginx -- netcat -q 2 -nzv $EXTERNAL_IP 80
-kubectl exec -n "$NS" -it external -- netcat -w 2 -zv www.k8s-school.fr 80
+kubectl exec -n "$NS" -it external -- netcat -w 2 -zv www.k8s-school.fr 443
 
 # Test network policies below
 KUBIA_DIR="/tmp/kubernetes-in-action"
