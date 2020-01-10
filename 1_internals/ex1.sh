189d940d64df75bdea265b7762705c154b6e7c0e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-01-10 16:05:50 +0100

    upd

--- 1_internals/ex1.sh
+++ 1_internals/ex1.sh
@@ -9,7 +9,7 @@ set -x
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 kubectl get pods -n kube-system
-ETCD_POD=$(kubectl get pods -n kube-system -l component=etcd,tier=control-plane -o jsonpath="{.items[0].metadata.name}")
+ETCD_POD=$(kubectl get pods -n kube-system -l component=etcd,tier=control-plane -o jsonpath='{.items[0].metadata.name}')
 
 kubectl exec -it -n kube-system "$ETCD_POD" --  \
     sh -c "ETCDCTL_API=3 etcdctl --cert /etc/kubernetes/pki/etcd/peer.crt \
