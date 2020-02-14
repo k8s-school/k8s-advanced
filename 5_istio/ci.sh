3d9976b23e0c3081ec167d632ad8c7271caea7de
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-02-14 11:17:48 +0100

    upd

--- 5_istio/ci.sh
+++ 5_istio/ci.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e
+set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-$DIR/ex1-securitycontext.sh
-$DIR/ex4-network.sh
+$DIR/istio-install.sh
+$DIR/bookinfo-install.sh
+$DIR/bookinfo-query.sh
+$DIR/istio-uninstall.sh
