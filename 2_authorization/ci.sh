dce92a81b4bf99c0f85d932de6861de1bbdb1340
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-02-14 16:27:38 +0100

    upd

--- 2_authorization/ci.sh
+++ 2_authorization/ci.sh
@@ -7,6 +7,12 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 FILES=$DIR/*.sh
 for f in $FILES
 do
+  if echo "$f" | grep "ci\.sh"; then
+      echo
+      echo "-------------------------------------------------------------------"
+      echo "NOT processing $f"
+      echo "-------------------------------------------------------------------"
+  else
       echo
       echo "-------------------------------------------------------------------"
       echo "Processing $f"
