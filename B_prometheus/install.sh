577e89506999a6886836052db10d2774c43272b4
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-27 17:25:48 +0200

    upd

--- B_prometheus/install.sh
+++ B_prometheus/install.sh
@@ -14,7 +14,7 @@ kubectl create namespace "$NS"
 kubectl label ns "$NS" "name=$NS"
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || echo "Unable to add repo prometheus-community"
-helm repo add stable https://charts.helm.sh/stable || echo "Unable to add repo stable"
+helm repo add stable https://charts.helm.sh/stable --force-update
 helm repo update
 helm install --version 12.2.2 prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring
 
