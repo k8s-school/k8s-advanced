32e34a53a953e4e135dc1362c9ac39784b54b892
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-05-01 08:57:52 +0200

    Update kubeadm/containerd install script

--- 0_kubeadm/resource/ubuntu/prereq.sh
+++ 0_kubeadm/resource/ubuntu/prereq.sh
@@ -1,79 +1,83 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR/../env.sh"
 
-apt-get update -q
-
-# kubeadm
-##
-apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
 sudo apt-get update -q
-apt-get install -y --allow-downgrades --allow-change-held-packages \
-    kubelet="$KUBEADM_VERSION" kubeadm="$KUBEADM_VERSION" kubectl="$KUBEADM_VERSION"
-apt-mark hold kubelet kubeadm kubectl
 
-apt-get install -y ipvsadm
-
-# containerd
-##
-
-## Pre-requisites
-cat > /etc/modules-load.d/containerd.conf <<EOF
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
-modprobe overlay
-modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
 # Setup required sysctl params, these persist across reboots.
-cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sysctl --system
+# Apply sysctl params without reboot
+sudo sysctl --system
 
+# Install containerd
 ## Set up the repository
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-### Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+### Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 ### Add Docker apt repository.
-add-apt-repository \
-	    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	    $(lsb_release -cs) \
-            stable"
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 ## Install containerd
-apt-get update && apt-get install -y containerd.io
+sudo apt-get update -q
+sudo apt-get install -y containerd.io
 
 # Configure containerd
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
 
 # Restart containerd
-systemctl restart containerd
+sudo systemctl restart containerd
+
+# kubeadm
+##
+sudo apt-get update 
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+apt-get install -y --allow-downgrades --allow-change-held-packages \
+    kubelet="$KUBEADM_VERSION" kubeadm="$KUBEADM_VERSION" kubectl="$KUBEADM_VERSION"
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+sudo apt-get install -y ipvsadm
 
 # Configure crictl client
-cat > /etc/crictl.yaml <<EOF
+sudo cat > /etc/crictl.yaml <<EOF
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
 EOF
 
 # Helm
 #
-HELM_VERSION=3.3.3
+HELM_VERSION=3.5.4
 wget -O /tmp/helm.tgz https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 cd /tmp
 tar zxvf /tmp/helm.tgz
