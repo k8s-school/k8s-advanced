6fc248cb81ba91c3bcb0257e77f1c18b93a1ef58
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-10-06 23:20:28 +0200

    upd

--- 1_internals/ex1.sh
+++ 1_internals/ex1.sh
@@ -9,7 +9,7 @@ set -x
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 kubectl get pods -n kube-system
-kubectl  wait --timeout=120s --for=condition=Ready -n kube-system pods -l component=etcd,tier=control-plane
+kubectl  wait --timeout=240s --for=condition=Ready -n kube-system pods -l component=etcd,tier=control-plane
 kubectl get pods -n kube-system
 ETCD_POD=$(kubectl get pods -n kube-system -l component=etcd,tier=control-plane -o jsonpath='{.items[0].metadata.name}')
 
