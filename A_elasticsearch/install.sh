078af4d6959633c7d757b2afb121fb83be76e432
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-02-17 21:13:39 +0100

    Install EFK 7.17.3

--- A_elasticsearch/install.sh
+++ A_elasticsearch/install.sh
@@ -7,9 +7,9 @@ set -euxo pipefail
 
 NS="logging"
 
-helm delete elasticsearch || echo "elasticsearch not found"
-helm delete fluentd || echo "fluentd not found"
-helm delete kibana || echo "kibana not found"
+helm delete -n $NS elasticsearch || echo "elasticsearch not found"
+helm delete -n $NS fluentd || echo "fluentd not found"
+helm delete -n $NS kibana || echo "kibana not found"
 
 kubectl delete ns -l name="logging"
 kubectl create namespace "$NS"
@@ -18,16 +18,20 @@ kubectl label ns "$NS" name="logging"
 helm repo add elastic https://helm.elastic.co
 helm repo update
 
+# Fail due to security enhancement
+VERSION=8.5.1
+VERSION=7.17.3
+
 # Install elasticsearch
-helm install elasticsearch elastic/elasticsearch --namespace "$NS" --set data.terminationGracePeriodSeconds=0
+helm install --version "$VERSION" elasticsearch elastic/elasticsearch --namespace "$NS" --set data.terminationGracePeriodSeconds=0
 # \
 #    --set master.persistence.enabled=false --set data.persistence.enabled=false
 
 # Install fluentd
-helm install fluentd --namespace "$NS" elastic/filebeat
+helm install fluentd --version "$VERSION" --namespace "$NS" elastic/filebeat
 
 # Install Kibana
-helm install kibana --namespace "$NS" elastic/kibana
+helm install kibana --version "$VERSION" --namespace "$NS" elastic/kibana
 
 # Generate logs
 ./generate-log.sh > /dev/null &
@@ -38,4 +42,5 @@ POD_NAME=$(kubectl get pods --namespace "$NS" -l "app=kibana,release=kibana" -o 
 kubectl wait -n "$NS" --for=condition=Ready pods "$POD_NAME"
 
 kubectl port-forward -n "$NS" "$POD_NAME" 5601 &
-# In Kibana, go to "Discover", add "logstash*" index and "@timestamp" filter, then go to "Discover"
+echo 'In Kibana, go to "Discover", add "filebeat-7.17.3*" index and "@timestamp" filter'
+echo 'then go to "Discover" and search on "Connecting"'
