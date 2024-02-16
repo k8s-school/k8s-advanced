71f92ddaef26463c0ad27b5abb02d8b1f3375c26
Your Name <you@example.com>
2024-02-16 22:36:18 +0100

    Update current feature

--- 0_kubeadm/env.sh
+++ 0_kubeadm/env.sh
@@ -3,13 +3,13 @@ DISTRIB="ubuntu"
 
 
 MASTER="clus0-0"
-NODES="clus0-1 clus0-2"
+NODES="clus0-1"
 
 USER=fabrice_jammes_gmail_com
 # USER=k8sstudent_gmail_com
 
 gcloud config set project "coastal-sunspot-206412"
-ZONE="asia-east1-c"
+ZONE="us-central1-a"
 gcloud config set compute/zone $ZONE
 
 SCP="gcloud compute scp"
