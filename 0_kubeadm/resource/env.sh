9db6b726b65f148d84380b42ce1d472a95584305
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2024-03-24 21:25:54 +0100

    Update current feature

--- 0_kubeadm/resource/env.sh
+++ 0_kubeadm/resource/env.sh
@@ -1,4 +1,4 @@
-KUBEADM_VERSION="1.25.6-00"
+KUBEADM_VERSION="1.28.6-00"
 
 # Current version
 # KUBEADM_VERSION="1.25.2-00"
@@ -6,8 +6,8 @@ KUBEADM_VERSION="1.25.6-00"
 # apt-cache madison kubeadm
 
 # Required for update procedure
-BUMP_KUBEADM="1.26.1-00"
-BUMP_K8S="v1.26.1"
+BUMP_KUBEADM="1.29.1-00"
+BUMP_K8S="v1.29.1"
 
 # Remove debconf messages
 export TERM="linux"
