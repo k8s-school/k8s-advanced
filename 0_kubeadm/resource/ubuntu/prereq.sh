8607949ff3745816784730b84544a5ed06db6f2d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2024-03-24 21:28:43 +0100

    Update current feature

--- 0_kubeadm/resource/ubuntu/prereq.sh
+++ 0_kubeadm/resource/ubuntu/prereq.sh
@@ -62,7 +62,7 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p /etc/apt/keyrings
 sudo rm -f /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --no-tty --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
