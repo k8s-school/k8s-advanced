5e37f13eb2a82afb4dfc343df126f03d529031da
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-08-08 08:35:36 +0200

    Upgrade kubeadm

--- labs/0_kubeadm/resource/env.sh
+++ labs/0_kubeadm/resource/env.sh
@@ -1,5 +1,5 @@
-K8S_VERSION="v1.29"
-KUBEADM_VERSION="1.29.3-1.1"
+K8S_VERSION="v1.32"
+KUBEADM_VERSION="1.32.7-1.1"
 
 # Current version
 # KUBEADM_VERSION="1.25.2-00"
@@ -7,8 +7,8 @@ KUBEADM_VERSION="1.29.3-1.1"
 # apt-cache madison kubeadm
 
 # Required for update procedure
-BUMP_KUBEADM="1.29.4-2.1"
-BUMP_K8S="v1.29.4"
+BUMP_KUBEADM="1.33.3-1.1"
+BUMP_K8S="v1.33.3"
 
 # Remove debconf messages
 export TERM="linux"
