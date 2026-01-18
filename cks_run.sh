76572a5df57a262e28b39c80320c59ba22eb2dca
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-01-18 17:42:16 +0100

    Update current feature

---
+++ cks_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Starting execution of all CKS scripts from directories 5_ and above..."
+
+$DIR/labs/5_runtime_security/falco.sh
+$DIR/labs/6_security_hardening/kube-bench.sh
+$DIR/labs/7_supply_chain/1-trivy.sh
+$DIR/labs/7_supply_chain/2-cosign.sh
+# Create a new k8s cluster
+$DIR/labs/6_security_hardening/seccomp.sh
+
+echo "All scripts completed successfully!"
